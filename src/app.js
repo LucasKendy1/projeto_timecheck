96373886f5ca77cba40ada1efd939db79447a315
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2023-06-15 12:31:10 -0300

    adicionei novos recursos de banco de dados

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'fatecetop2023',
+  database: 'time_check',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados!');
+  // Exemplo de consulta SQL
+  connection.query('SELECT * FROM time_check.users', (err, rows) => {
+    if (err) {
+        console.error('Erro ao executar a consulta:', err);
+        return;
+    }
+
+    console.log('Resultados da consulta:', rows);
+  });
+
+  connection.end(); // Encerra a conexão com o banco de dados
+});
